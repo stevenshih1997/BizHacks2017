682725e51d88ae5e9b0d35f096e16588a93422c7
grigorii <prb578@icloud.com>
2017-03-31 21:15:35 -0700

    Basic authentication

--- app/src/main/java/com/location/bizhacks/storelocator/MainActivity.java
+++ app/src/main/java/com/location/bizhacks/storelocator/MainActivity.java
@@ -1,13 +1,80 @@
 package com.location.bizhacks.storelocator;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.firebase.ui.auth.AuthUI;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
+    private static final String ANONYMOUS = "Anonymous";
+    public static final int RC_SIGN_IN = 1;
+    private String mUsername;
+
+    private FirebaseAuth mFirebaseAuth;
+    private FirebaseAuth.AuthStateListener authStateListener;
+    TextView mMainText;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mFirebaseAuth = FirebaseAuth.getInstance();
+
+        mMainText = (TextView) findViewById(R.id.main_text);
+        authStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+
+                if (user != null) {
+                    Toast.makeText(MainActivity.this, "You're signed in. Welcome!", Toast.LENGTH_SHORT).show();
+                    onSignedInInitialize(user.getDisplayName());
+                } else {
+                    onSignedInCleanup();
+                    startActivityForResult(AuthUI.getInstance()
+                    .createSignInIntentBuilder()
+                    .setProviders(Arrays.asList(
+                            new AuthUI.IdpConfig.Builder(AuthUI.EMAIL_PROVIDER).build(),
+                            new AuthUI.IdpConfig.Builder(AuthUI.GOOGLE_PROVIDER).build()))
+                            .build(),
+                            RC_SIGN_IN
+                            );
+                }
+            }
+        };
+    }
+
+    public void onSignedInInitialize(String userName) {
+        mUsername = userName;
+        mMainText.setText(mUsername);
+    }
+
+    public void onSignedInCleanup() {
+        mUsername = ANONYMOUS;
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mFirebaseAuth.addAuthStateListener(authStateListener);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (authStateListener != null) {
+            mFirebaseAuth.removeAuthStateListener(authStateListener);
+        }
+
     }
 }
