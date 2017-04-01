3a25763927723e54e19918ac6740afab35298ed7
Steven Shih <stevenshih1997@gmail.com>
2017-03-31 20:58:09 -0700

    started API

--- app/src/main/java/com/location/bizhacks/storelocator/MainActivity.java
+++ app/src/main/java/com/location/bizhacks/storelocator/MainActivity.java
@@ -1,13 +1,34 @@
 package com.location.bizhacks.storelocator;
 
+import android.Manifest;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    private final int CODE_PERMISSIONS = //...
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        String[] neededPermissions = {
+                Manifest.permission.CHANGE_WIFI_STATE,
+                Manifest.permission.ACCESS_WIFI_STATE,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+        };
+        ActivityCompat.requestPermissions( this, neededPermissions, CODE_PERMISSIONS );
     }
+
+//...
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        //Handle if any of the permissions are denied, in grantResults
+    }
+
+
 }
