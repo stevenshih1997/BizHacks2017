de70ffd43bc8c6e60526e9ae2ee078293b0f3ea6
Steven Shih <stevenshih1997@gmail.com>
2017-03-31 21:15:10 -0700

    latest update

--- app/src/main/java/com/location/bizhacks/storelocator/MainActivity.java
+++ app/src/main/java/com/location/bizhacks/storelocator/MainActivity.java
@@ -5,20 +5,32 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.indooratlas.android.sdk.IALocationListener;
+import com.indooratlas.android.sdk.IALocationManager;
+
 public class MainActivity extends AppCompatActivity {
 
 
-    private final int CODE_PERMISSIONS = //...
+    private final int CODE_PERMISSIONS = 225;//...
+    private IALocationManager mIALocationManager; //Register and unregister for periodic updates of the user's current location.
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
         String[] neededPermissions = {
                 Manifest.permission.CHANGE_WIFI_STATE,
                 Manifest.permission.ACCESS_WIFI_STATE,
                 Manifest.permission.ACCESS_COARSE_LOCATION
         };
         ActivityCompat.requestPermissions( this, neededPermissions, CODE_PERMISSIONS );
+
+        mIALocationManager = IALocationManager.create(this);
+
+
     }
 
 //...
@@ -30,5 +42,17 @@ public class MainActivity extends AppCompatActivity {
         //Handle if any of the permissions are denied, in grantResults
     }
 
+
+
+    private IALocationListener mIALocationListener = new IALocationListener() {
+
+        // Called when the location has changed.
+        @Override
+        public void onLocationChanged(IALocation location) {
+
+            Log.d(TAG, "Latitude: " + location.getLatitude());
+            Log.d(TAG, "Longitude: " + location.getLongitude());
+        }
+    };
 
 }
